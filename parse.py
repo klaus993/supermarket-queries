a87cdc3baf0e3b14d84492374e51b28f51eb78c9
klaus993 <lungwitz.klaus@gmail.com>
2016-04-29 00:25:36 -0300

    parse functions added

--- parse.py
+++ parse.py
@@ -1,11 +1,28 @@
 import csv
-
+from time import sleep
 
 def parse_to_dict(file):
+    """Takes a csv file and parses it into a
+    dictionary. Pre condition: the csv file has
+    to have only two fields. Returns the dictionary.
+    """
     dic = {}
     with open(file) as f:
-        csv_super = csv.reader(f)
-        next(csv_super)
-        for fila in csv.reader(f):
-            dic[fila[0]] = fila[1]
+        rows = csv.reader(f)
+        next(rows)
+        for row in rows:
+            dic[row[0]] = row[1]
+    return dic
+
+
+def prices_to_dict(file, supermarket_id, product_id):
+    dic = {}
+    with open(file) as f:
+        rows = csv.reader(f)
+        next(rows)
+        for row in rows:
+            if row[0] == supermarket_id and row[1] == product_id:
+                print(row)
+                sleep(0.1)
+                dic[row[2]] = row[3]
     return dic

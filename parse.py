3f0a552e81826c4ba8700149e88897d93116b2ee
klaus993 <lungwitz.klaus@gmail.com>
2016-04-28 23:44:00 -0300

    parse simple csv files

--- parse.py
+++ parse.py
@@ -1,5 +1,11 @@
-# import csv
+import csv
 
 
-def products(file):
-    pass
+def parse_to_dict(file):
+    dic = {}
+    with open(file) as f:
+        csv_super = csv.reader(f)
+        next(csv_super)
+        for fila in csv.reader(f):
+            dic[fila[0]] = fila[1]
+    return dic

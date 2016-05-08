296d7c153b25160448ad5222fe45d146a138f119
klaus993 <lungwitz.klaus@gmail.com>
2016-05-07 23:40:39 -0300

    Added validations.

--- ui.py
+++ ui.py
@@ -9,14 +9,17 @@ def convert_period(period):
     """Converts user entered periods to the corresponding dict keys
     """
     if len(period) != 7 or period[2] != '-':
-        raise ValueError('sintaxis incorrecta.')
+        raise ValueError('sintaxis incorrecta (MM-AAAA).')
     return period[3:] + period[:2]
 
 
 def ask_and_convert(prices, string):
     """Asks the user for a starting and an ending period and returns the converted periods as a tuple.
     """
-    period = int(convert_period(input('Ingrese mes y año de {}: '.format(string))))
+    try:
+        period = int(convert_period(input('Ingrese mes y año de {}: '.format(string))))
+    except ValueError:
+        raise ValueError('período no encontrado.')
     if period not in prices.keys():
         raise ValueError('período no encontrado.')
     return period
@@ -64,11 +67,11 @@ def ask_prod(products):
     string = str()
     while not string:
         string = input('Ingrese el nombre de un producto: ')
-    for key in PRODUCTS:
-        if string in PRODUCTS[key]:
+    for key in products:
+        if string in products[key]:
             loop = True
             while loop:
-                is_prod = input('\n [{}]\n¿Es este el producto deseado? [s/n]: '.format(PRODUCTS[key]))
+                is_prod = input('\n [{}]\n¿Es este el producto deseado? [s/n]: '.format(products[key]))
                 if is_prod == 's' or is_prod == 'S':
                     return key
                 if is_prod != 'n' and is_prod != 'N':
@@ -85,12 +88,11 @@ def prod_inflation(supers, products, prices):
         try:
             start = ask_and_convert(prices, START)
             end = ask_and_convert(prices, END)
-            while True:
+            prod_id = int()
+            while not prod_id:
                 prod_id = ask_prod(products)
                 if not prod_id:
                     print('Producto no encontrado.')
-                else:
-                    break
             print()
             for sup_id in supers:
                 print('{}: {:.2f}%'.format(supers[sup_id], get_prod_inflation(start, end, prod_id, sup_id, prices)))
@@ -103,25 +105,24 @@ def prod_inflation(supers, products, prices):
 def best_price(supers, products, prices):
     """
     """
-    period = ask_and_convert(prices, 'búsqueda')
-    prod_id = ask_prod(products)
-    # best_price = (15.2, [1, 2, 3])
-    best_price = get_best_price(period, prod_id, prices)
-    for sup_id in best_price[1]:
-        print('\n{}'.format(supers[sup_id]), end=' ')
-    print(': ${}'.format(best_price[0]))
-    press_to_continue()
+    enter_loop = True
+    while enter_loop:
+        try:
+            period = ask_and_convert(prices, 'búsqueda')
+            prod_id = ask_prod(products)
+            best_price = get_best_price(period, prod_id, prices)
+            for sup_id in best_price[1]:
+                print('\n{}'.format(supers[sup_id]), end=' ')
+            print(': ${}'.format(best_price[0]))
+            press_to_continue()
+            enter_loop = False
+        except ValueError as e:
+            print('Error: ' + str(e))
 
 
-def print_menu():
+def print_menu(menu):
     """
     """
-    menu = {1: 'Inflación por supermercado',
-            2: 'Inflación por producto',
-            3: 'Inflación general promedio',
-            4: 'Mejor precio para un producto',
-            5: 'Salir'
-            }
     print('\nMenú principal')
     print('------')
     for option in menu:
@@ -130,9 +131,17 @@ def print_menu():
 
 
 def interactive_menu(supers, products, prices):
+    """
+    """
+    menu = {1: 'Inflación por supermercado',
+            2: 'Inflación por producto',
+            3: 'Inflación general promedio',
+            4: 'Mejor precio para un producto',
+            5: 'Salir'
+            }
     enter_loop = True
     while enter_loop:
-        print_menu()
+        print_menu(menu)
         try:
             choice = int(input('Opción: '))
         except ValueError:
